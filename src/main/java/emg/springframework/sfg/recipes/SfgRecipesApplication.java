e312ed8ba2748aae410c763ed310b2a297d3c191
petrubear <edison.martinez@gmail.com>
2020-08-07 17:06:35 -0500

    feat: Initial Import

---
+++ src/main/java/emg/springframework/sfg/recipes/SfgRecipesApplication.java
@@ -0,0 +1,13 @@
+package emg.springframework.sfg.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/emg/springframework/sfg/recipes/SfgRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package emg.springframework.sfg.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
