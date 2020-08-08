287ea5765cb6734f814c93d270551c5fd5e75864
petrubear <edison.martinez@gmail.com>
2020-08-07 19:24:32 -0500

    feat: add recipe dataloader

--- src/main/java/emg/springframework/sfg/recipes/controllers/IndexController.java
+++ src/main/java/emg/springframework/sfg/recipes/controllers/IndexController.java
@@ -1,9 +1,11 @@
 package emg.springframework.sfg.recipes.controllers;
 
 import emg.springframework.sfg.recipes.repositories.CategoryRepository;
+import emg.springframework.sfg.recipes.repositories.RecipeRepository;
 import emg.springframework.sfg.recipes.repositories.UnitOfMeasureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,20 +13,24 @@ public class IndexController {
 
     private final CategoryRepository categoryRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeRepository recipeRepository;
 
     @Autowired
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeRepository recipeRepository) {
         this.categoryRepository = categoryRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.recipeRepository = recipeRepository;
     }
 
     @RequestMapping( {"/", "", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
         var category = categoryRepository.findByDescription("American");
         var unit = unitOfMeasureRepository.findByDescription("Teaspoon");
+        //category.ifPresent(c -> System.out.println("category id = " + c.getId()));
+        //unit.ifPresent(u -> System.out.println("unit id = " + u.getId()));
 
-        category.ifPresent(c -> System.out.println("category id = " + c.getId()));
-        unit.ifPresent(u -> System.out.println("unit id = " + u.getId()));
+        var recipes = recipeRepository.findAll();
+        model.addAttribute("recipes", recipes);
         return "index";
     }
 }
