63a40dec0be6a71d3f70292dbb7b23d7af8423f3
petrubear <edison.martinez@gmail.com>
2020-08-07 19:02:07 -0500

    feat: add query methods

--- src/main/java/emg/springframework/sfg/recipes/controllers/IndexController.java
+++ src/main/java/emg/springframework/sfg/recipes/controllers/IndexController.java
@@ -1,14 +1,30 @@
 package emg.springframework.sfg.recipes.controllers;
 
+import emg.springframework.sfg.recipes.repositories.CategoryRepository;
+import emg.springframework.sfg.recipes.repositories.UnitOfMeasureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @RequestMapping({"/", "", "/index"})
-    public String getIndexPage(){
+    @Autowired
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping( {"/", "", "/index"})
+    public String getIndexPage() {
+        var category = categoryRepository.findByDescription("American");
+        var unit = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        category.ifPresent(c -> System.out.println("category id = " + c.getId()));
+        unit.ifPresent(u -> System.out.println("unit id = " + u.getId()));
         return "index";
     }
 }
--- src/main/java/emg/springframework/sfg/recipes/repositories/CategoryRepository.java
+++ src/main/java/emg/springframework/sfg/recipes/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package emg.springframework.sfg.recipes.repositories;
 import emg.springframework.sfg.recipes.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GategoryRepository extends CrudRepository<Category, Long> {
+import java.util.Optional;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
