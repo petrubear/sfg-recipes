a82e660d5a70d967981d6791d1cd22d69524b5be
petrubear <edison.martinez@gmail.com>
2020-08-14 17:52:06 -0500

    feat: update recipes

--- src/main/java/emg/springframework/sfg/recipes/controllers/RecipeController.java
+++ src/main/java/emg/springframework/sfg/recipes/controllers/RecipeController.java
@@ -17,7 +17,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
@@ -29,12 +29,19 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
+    @RequestMapping("/recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) {
+        var recipe = recipeService.findCommandById(Long.valueOf(id));
+        model.addAttribute("recipe", recipe);
+        return "recipe/recipeform";
+    }
+
     //@RequestMapping(name = "recipe", method = RequestMethod.POST)
     @PostMapping
     @RequestMapping("/recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
 
--- src/main/java/emg/springframework/sfg/recipes/services/RecipeServiceImpl.java
+++ src/main/java/emg/springframework/sfg/recipes/services/RecipeServiceImpl.java
@@ -48,4 +48,10 @@ public class RecipeServiceImpl implements RecipeService {
         log.debug("Saved Recipe Id: {}", savedRecipe.getId());
         return recipeToRecipeCommand.convert(savedRecipe);
     }
+
+    @Override
+    @Transactional
+    public RecipeCommand findCommandById(Long id) {
+        return recipeToRecipeCommand.convert(findById(id));
+    }
 }
