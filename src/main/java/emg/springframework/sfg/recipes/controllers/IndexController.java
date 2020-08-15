1331a216f7250130acad527509386957cb5fb065
petrubear <edison.martinez@gmail.com>
2020-08-15 15:02:40 -0500

    refactor: request mapping to getmapping

--- src/main/java/emg/springframework/sfg/recipes/controllers/IndexController.java
+++ src/main/java/emg/springframework/sfg/recipes/controllers/IndexController.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
@@ -25,7 +25,7 @@ public class IndexController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping( {"/", "", "/index"})
+    @GetMapping( {"/", "", "/index"})
     public String getIndexPage(Model model) {
         var category = categoryRepository.findByDescription("American");
         var unit = unitOfMeasureRepository.findByDescription("Teaspoon");
--- src/main/java/emg/springframework/sfg/recipes/controllers/RecipeController.java
+++ src/main/java/emg/springframework/sfg/recipes/controllers/RecipeController.java
@@ -4,7 +4,10 @@ import emg.springframework.sfg.recipes.commands.RecipeCommand;
 import emg.springframework.sfg.recipes.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecipeController {
@@ -14,22 +17,19 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/show")
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/new")
+    @GetMapping("/recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
         return "recipe/recipeform";
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/update")
+    @GetMapping("/recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
         var recipe = recipeService.findCommandById(Long.valueOf(id));
         model.addAttribute("recipe", recipe);
@@ -37,15 +37,13 @@ public class RecipeController {
     }
 
     //@RequestMapping(name = "recipe", method = RequestMethod.POST)
-    @PostMapping
-    @RequestMapping("/recipe")
+    @PostMapping("/recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/delete")
+    @GetMapping("/recipe/{id}/delete")
     public String deleteRecipe(@PathVariable String id) {
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
